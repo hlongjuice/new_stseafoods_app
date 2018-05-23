96ff4879dcd6fa5707020e1dc1d15ac2a8d4d747
Jubilus <isis.juicy@gmail.com>
2018-05-23 14:25:12 +0700

    1.0.13
    update ล่าสุด

--- src/services/weburl.service.ts
+++ src/services/weburl.service.ts
@@ -6,9 +6,10 @@ export class WebUrlService{
     constructor(
         // private url:string
     ){
-        this.url='http://192.168.43.241/stseafood/public';
+        // this.url='http://192.168.43.241/stseafood/public';
+        // this.url='http://127.0.0.1/stseafood/public';
         //Factory IP
-        // this.url='http://192.168.2.13:8100/stseafood/public';
+        this.url='http://192.168.2.13:8100/stseafood/public';
         //End Factory IP
     }
     getUrl(){
